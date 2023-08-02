826cda22ae8dcb02e0907bed2134570eb142604b
mzahin01 <minhazahin@gmail.com>
2023-08-02 15:16:52 +0600

    7

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class Appli extends StatelessWidget {
         body: Container(
           height: MediaQuery.of(context).size.height / 2,
           width: MediaQuery.of(context).size.width,
-          margin: const EdgeInsets.all(20),
+          margin: const EdgeInsets.all(30),
           child: GridView.builder(
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2),
