b4800fe39461e071a9f65439f05fb0195c63c809
mzahin01 <minhazahin@gmail.com>
2023-08-02 13:14:45 +0600

    2nd?ish commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hope/ptwo.dart';
+import 'package:hope/pthree.dart';
 
 main() {
   runApp(const MaterialApp(
@@ -12,9 +14,25 @@ class Appli extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-      ),
-    );
+        appBar: AppBar(title: const Text("Hudai")),
+        body: Container(
+          height: MediaQuery.of(context).size.height / 2,
+          width: MediaQuery.of(context).size.height / 2,
+          margin: const EdgeInsets.all(8),
+          child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              itemCount: 2,
+              itemBuilder: (context, int index) {
+                return _box(index);
+              }),
+        ));
+  }
+
+  Widget _box(int index) {
+    return Container(color: index / 2 == 0 ? Colors.red : Colors.green);
+    onPressed() {
+      if (index == 1) {}
+    }
   }
 }
